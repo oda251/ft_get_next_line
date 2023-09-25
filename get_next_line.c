fb17f71f2812bee33af6ed7e6093991b25975008
oda251 <69433701+oda251@users.noreply.github.com>
2023-09-25 21:59:34 +0000

    a

--- get_next_line.c
+++ get_next_line.c
@@ -3,45 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoda <yoda@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: yoda <yoda@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 18:54:07 by yoda              #+#    #+#             */
-/*   Updated: 2023/09/24 19:58:54 by yoda             ###   ########.fr       */
+/*   Updated: 2023/09/25 21:58:46 by yoda             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*str_push_back(char *str, char c, size_t len);
-
 char	*get_next_line(int fd)
 {
-	char	*dest;
-	char	*buf;
-	size_t	len;
-	int		r;
+	static char	*read_log[FOPEN_MAX] = {NULL};
+	char		*dest;
+	char		*buf;
 
-	dest = malloc(1);
-	*dest = '\0';
-	if (!dest)
+	if (!initialize_get_next_line(fd, &dest, &buf, &read_log[fd]))
 		return (NULL);
-	len = 0;
-	while (1)
+	if (!cat_to_line_break(&dest, read_log[fd], read_log[fd], BUFFER_SIZE))
 	{
-		r = read(fd, buf, 1);
-		if (r < 0)
-		{
-			if (len == 0)
-				return (NULL);
-			break ;
-		}
-		printf("%c", *buf);
-		dest = str_push_back(dest, *buf, len);
-		if (!dest)
-			return (NULL);
-		if (*buf == '\n')
-			break ;
-		len++;
+		free(buf);
+		return (NULL);
+	}
+	solve_get_next_line(fd, &dest, buf, read_log[fd]);
+	free(buf);
+	if (!dest)
+	{
+		free(read_log[fd]);
+		return (NULL);
+	}
+	if (!*dest)
+	{
+		free(dest);
+		free(read_log[fd]);
+		return (NULL);
 	}
 	return (dest);
 }
+
+//#include <fcntl.h>
+//int main()
+//{
+//	char *line;
+//	char *line2;
+//	char *line3;
+//	int fd = open("test.txt", O_RDONLY);
+//	int fd2 = open("test2.txt", O_RDONLY);
+//	int fd3 = open("test3.txt", O_RDONLY);
+//	while (1)
+//	{
+//		line = get_next_line(fd);
+//		line2 = get_next_line(fd2);
+//		line3 = get_next_line(fd3);
+//		if (!line)
+//			break;
+//		printf("%s", line);
+//		printf("%s", line2);
+//		printf("%s", line3);
+//		free(line);
+//		free(line2);
+//		free(line3);
+//	}
+//	close(fd);
+//	return (0);
+//}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,34 +3,126 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yoda <yoda@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: yoda <yoda@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 19:04:31 by yoda              #+#    #+#             */
-/*   Updated: 2023/09/24 19:59:13 by yoda             ###   ########.fr       */
+/*   Updated: 2023/09/25 21:58:32 by yoda             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*str_push_back(char *str, char c, size_t len)
+static char	*realloc_cat(char **str, char *src, size_t len_str, size_t n)
 {
 	char	*dest;
-	size_t	i;
 
-	dest = malloc(len + 2);
-	if (!dest)
+	if (SIZE_MAX - len_str <= n)
 	{
-		free(str);
+		free(*str);
 		return (NULL);
 	}
-	dest[len + 1] = '\0';
-	i = 0;
-	while (i < len)
+	dest = malloc(len_str + n + 1);
+	if (!dest)
 	{
-		dest[i] = str[i];
-		i++;
+		free(*str);
+		return (NULL);
 	}
-	dest[i] = c;
-	free(str);
+	dest[len_str + n] = '\0';
+	while (--n != SIZE_MAX)
+		dest[len_str + n] = src[n];
+	while (--len_str != SIZE_MAX)
+		dest[len_str] = (*str)[len_str];
+	free(*str);
 	return (dest);
+}
+
+static int	search_line_break(char *buf, size_t r)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < r && buf[i])
+	{
+		if (buf[i++] == '\n')
+			return (1);
+	}
+	return (0);
+}
+
+int	initialize_get_next_line(int fd, char **dest, char **buf, char **read_log)
+{
+	if (fd < 0 || fd >= FOPEN_MAX || BUFFER_SIZE <= 0)
+		return (0);
+	*dest = malloc(1);
+	if (!*dest)
+		return (0);
+	**dest = '\0';
+	*buf = malloc(BUFFER_SIZE);
+	if (!*buf)
+	{
+		free(*dest);
+		return (0);
+	}
+	if (!*read_log)
+	{
+		*read_log = malloc(BUFFER_SIZE);
+		if (!*read_log)
+		{
+			free(*dest);
+			free(*buf);
+			return (0);
+		}
+		**read_log = '\0';
+	}
+	return (1);
+}
+
+int	cat_to_line_break(char **dest, char *src, char *log, size_t r)
+{
+	size_t	i;
+	size_t	tmp;
+	size_t	len_dest;
+
+	len_dest = 0;
+	while ((*dest)[len_dest])
+		len_dest++;
+	i = 0;
+	while (i < r && src[i])
+	{
+		if (src[i++] == '\n')
+			break ;
+	}
+	*dest = realloc_cat(dest, src, len_dest, i);
+	if (!*dest)
+	{
+		free(*dest);
+		return (0);
+	}
+	tmp = i--;
+	while (++i < r && src[i])
+		log[i - tmp] = src[i];
+	log[i - tmp] = '\0';
+	return (1);
+}
+
+void	solve_get_next_line(int fd, char **dest, char *buf, char *read_log)
+{
+	size_t	r;
+	int		line_break_detected;
+
+	line_break_detected = 0;
+	while (!line_break_detected)
+	{
+		r = read(fd, buf, BUFFER_SIZE);
+		if (r < 0)
+		{
+			free(*dest);
+			*dest = NULL;
+			return ;
+		}
+		else if (r == 0)
+			break ;
+		line_break_detected = search_line_break(buf, r);
+		cat_to_line_break(dest, buf, read_log, r);
+	}
 }
